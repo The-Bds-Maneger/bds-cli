4087966c666afaa75f2c0e07f13a2bd386c197d6
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-05-06 16:37:57 +0000

    Add bin build

--- buildBin.js
+++ buildBin.js
@@ -7,6 +7,7 @@ const options = {
   input: path.resolve(__dirname, "./dist/index.js"),
   resources: fs.readdirSync(path.resolve(__dirname, "./dist/")).map(file => path.resolve(__dirname, "./dist/", file)),
   output: path.resolve(process.cwd(), `bin_${process.platform}_${process.arch}${process.platform === "win32" ? ".exe" : ""}`),
+  targets: (() => {let a = process.argv.find(arg => arg.includes("--target="));if (!!a) return a.replace("--target=", ""); return undefined;})()
 };
 
 nexe.compile(options).catch(err => {
