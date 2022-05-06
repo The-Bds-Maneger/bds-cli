a0cee2c80571275c93bbfe144a0b27e470949cb0
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-05-06 20:00:17 +0000

    Update main

--- buildBin.js
+++ buildBin.js
@@ -9,22 +9,19 @@ const options = {
   output: path.resolve(process.cwd(), `bin_${process.platform}_${process.arch}${process.platform === "win32" ? ".exe" : ""}`),
   targets: (() => {let a = process.argv.find(arg => arg.includes("--target="));if (!!a) return a.replace("--target=", ""); return undefined;})(),
   configure: (() => {
-    if (process.argv.includes("--config_args=")) {
-      const confArgs = process.argv.find(arg => arg.includes("--config_args="));
-      if (/".*"/.test(confArgs)) {
-        return confArgs.replace("--config_args=", "").match(/"(.*)"/)[1].split(/,/).map(a => a.trim());        
-      } else {
-        return confArgs.replace("--config_args=", "").split(/,/).map(a => a.trim());
-      }
+    const confArgs = process.argv.find(arg => arg.includes("--config_args="));
+    if (!!confArgs) {
+      if (/".*"/.test(confArgs)) return confArgs.replace("--config_args=", "").match(/"(.*)"/)[1].split(/,/).map(a => a.trim());
+      else return confArgs.replace("--config_args=", "").split(/,/).map(a => a.trim());
     }
     return undefined;
   })()
 };
 
+if (process.argv.includes("--verbose")) console.log("Base config:\n", options);
 nexe.compile(options).catch(err => {
   if (String(err).includes("--build")) {
     console.log("Attempting to build for current platform...");
-    console.log("Base config:\n", options);
     return nexe.compile({
       ...options,
       build: true,
