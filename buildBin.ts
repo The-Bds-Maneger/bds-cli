2acafb93dd3a836911528d2e9ef242a7da752d7c
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-07-06 17:02:18 +0000

    Update ci and build bins

--- buildBin.ts
+++ buildBin.ts
@@ -1,25 +1,32 @@
 #!/usr/bin/env node
-const nexe = require("nexe");
-const path = require("path");
-
+import * as path from "node:path";
+import * as nexe from "nexe";
 let Platform = process.platform, Arch = process.arch;
-const Target = (() => {let a = process.argv.find(arg => arg.includes("--target="));if (!!a) return a.replace("--target=", ""); return undefined;})();
-const Configure = (() => {
-  const confArgs = process.argv.find(arg => arg.includes("--config_args="));
+
+const Target = ((): undefined|string => {let a = process.argv.find(arg => arg.includes("--target="));if (!!a) return a.replace("--target=", ""); return undefined;})();
+const Configure = ((): undefined|string[] => {
+  let confArgs = process.argv.find(arg => arg.includes("--config_args="));
   if (!!confArgs) {
-    if (/".*"/.test(confArgs)) return confArgs.replace("--config_args=", "").match(/"(.*)"/)[1].split(/,/).map(a => a.trim());
-    else return confArgs.replace("--config_args=", "").split(/,/).map(a => a.trim());
+    confArgs = confArgs.replace("--config_args=", "");
+    if (/".*"/.test(confArgs)) {
+      let testArgs = confArgs.match(/"(.*)"/);
+      if (!!testArgs) confArgs = testArgs[1];
+    }
+    return confArgs.split(/\s+?,\s+/).map(a => a.trim());
   }
   return undefined;
 })();
-if (/android/.test(Target)) Platform = "android";
-else if (/linux/.test(Target)) Platform = "linux";
-if (/aarch64|arm64/.test(Target)) Arch = "arm64";
+
+if (!!Target) {
+  if (/android/.test(Target)) Platform = "android";
+  else if (/linux/.test(Target)) Platform = "linux";
+  if (/aarch64|arm64/.test(Target)) Arch = "arm64";
+}
 
 const FileName = `BdsManegerCLI_${Platform}_${Arch}${process.platform === "win32" ? ".exe" : ""}`;
-const options = {
+const options: Partial<nexe.NexeOptions> = {
   input: path.resolve(__dirname, "./dist/index.js"),
-  targets: Target,
+  targets: !!Target? [Target]:undefined,
   resources: [
     path.resolve(__dirname, "package*.json"),
     path.resolve(__dirname, "./dist")
