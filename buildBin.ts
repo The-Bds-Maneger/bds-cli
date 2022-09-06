3b342f286d9fd2b11622672dbb8d27e0c9b61dc8
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-09-06 18:09:51 -0300

    Change files and folder

--- buildBin.ts
+++ buildBin.ts
@@ -1,48 +1,53 @@
 #!/usr/bin/env node
 import * as path from "node:path";
 import * as nexe from "nexe";
-let Platform = process.platform, Arch = process.arch;
+import yargs from "yargs";
 
-const Target = ((): undefined|string => {let a = process.argv.find(arg => arg.includes("--target="));if (!!a) return a.replace("--target=", ""); return undefined;})();
-const Configure = ((): undefined|string[] => {
-  let confArgs = process.argv.find(arg => arg.includes("--config_args="));
-  if (!!confArgs) {
-    confArgs = confArgs.replace("--config_args=", "");
-    if (/".*"/.test(confArgs)) {
-      let testArgs = confArgs.match(/"(.*)"/);
-      if (!!testArgs) confArgs = testArgs[1];
-    }
-    return confArgs.split(/\s+?,\s+/).map(a => a.trim());
-  }
-  return undefined;
-})();
+const Yargs = yargs(process.argv.slice(2)).help().version(false).alias("h", "help").wrap(yargs.terminalWidth()).option("Arch", {
+  alias: "a",
+  type: "string",
+  default: process.arch,
+  description: "Target arch"
+}).option("Platform", {
+  alias: "p",
+  type: "string",
+  default: process.platform,
+  description: "Target OS/Platform"
+}).option("conf", {
+  alias: "c",
+  type: "array",
+  default: [],
+  description: "Extra config to nexe"
+}).option("verbose", {
+  type: "boolean",
+  alias: "VV",
+  default: false
+}).option("build", {
+  alias: "b"
+}).parseSync();
 
-if (!!Target) {
-  if (/android/.test(Target)) Platform = "android";
-  else if (/linux/.test(Target)) Platform = "linux";
-  if (/aarch64|arm64/.test(Target)) Arch = "arm64";
-}
+const { Arch, Platform, conf } = Yargs;
 
-const FileName = `BdsManegerCLI_${Platform}_${Arch}${process.platform === "win32" ? ".exe" : ""}`;
+const FileName = `BdsManegerCLI_${Platform}_${Arch}${Platform === "win32" ? ".exe" : ""}`;
 const options: Partial<nexe.NexeOptions> = {
   input: path.resolve(__dirname, "./dist/index.js"),
-  targets: !!Target? [Target]:undefined,
+  targets: [`${Platform}_${Arch}`],
   resources: [
     path.resolve(__dirname, "package*.json"),
     path.resolve(__dirname, "./dist")
   ],
   output: path.resolve(process.cwd(), FileName),
-  configure: Configure
+  configure: !conf?[]:conf
 };
 
-if (process.argv.includes("--verbose")) console.log("Base config:\n", options);
+if (Yargs.verbose) console.log("Base config:\n", options);
 nexe.compile(options).catch(err => {
   if (String(err).includes("--build")) {
     console.log("Attempting to build for current platform...");
     return nexe.compile({
       ...options,
       build: true,
-      verbose: process.argv.includes("--verbose"),
+      verbose: Yargs.verbose,
       python: process.platform === "win32" ? "python.exe" : "python3"
     });
   }
